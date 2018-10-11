5679d935eb22550320fe0a74027cdace7fed20d5
kanishka kuksal <37463477+kukshalkanishka@users.noreply.github.com>
2018-10-11 23:20:23 +0530

    Test modified

--- insert_value_in_btw_formedArray.js
+++ insert_value_in_btw_formedArray.js
@@ -1,29 +1,18 @@
-let assert = require("assert");
-
-const copyNumbers = function(numbers, numPosition, copiedNumbers, copyToPosition, copyTillPosition){
-  let copiedNumIndex = copyToPosition;
-  for(let position = numPosition; position < copyTillPosition; position++) {
-    copiedNumbers[copiedNumIndex] = numbers[position];
-    copiedNumIndex++;
-  }
-  return copiedNumbers;
-}
-
-const insertValue = function(numbers, numPosition, value) {
-  let copiedNumbers = [];
-  copiedNumbers = copyNumbers(numbers, 0, copiedNumbers, 0, numPosition);
-  copiedNumbers[numPosition] = value;
-  copiedNumbers = copyNumbers( numbers, numPosition, copiedNumbers, numPosition +1, numbers.length);
-  
-  return copiedNumbers;
-}
+let insertValue = require("./library_insert_value.js").insertValue;
+let assert = require("assert")
 
 const main = function(){
-  let numbers = [1,4,7,8,9];
-  let output = insertValue(numbers,3,0);
-  console.log(output);
+
+  let output = insertValue([1,4,7,8,9],3,0);
   assert.deepEqual(output,[1,4,7,0,8,9]);
+  output = insertValue([1,4,7,8,9],0,0);
+  assert.deepEqual(output,[0,1,4,7,8,9]);
+  output = insertValue([1,4,7,8,9],2,0);
+  assert.deepEqual(output,[1,4,0,7,8,9]);
+  output = insertValue([1,4,7,8,9],4,2);
+  assert.deepEqual(output,[1,4,7,8,2,9]);
   return;
+
 }
 
 main();
